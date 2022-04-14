414758cd5c31ed1a103a2b93da273d4cc2c9364c
D4C-lolu <dareolumideisfly@gmail.com>
2022-04-14 14:06:04 +0100

    Chore: Added README file

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,8 +1,12 @@
 package config
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
 )
 
 var (
@@ -10,8 +14,20 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "demilade:kurohitsugi@tcp(127.0.0.1:3306)/bookstore?charset=utf8&parseTime=True&loc=Local")
+	err := godotenv.Load("C:\\Users\\Demilade\\Documents\\web_projects\\Golang\\book_store\\.env")
 	if err != nil {
+		fmt.Printf("%s", err)
+		panic(err)
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbURL := os.Getenv("DB_URL")
+	dbName := os.Getenv("DB_NAME")
+	dbString := fmt.Sprintf("%s:%s@tcp(%s)", dbUser, dbPassword, dbURL)
+	d, err := gorm.Open("mysql", fmt.Sprintf("%s/%s?charset=utf8&parseTime=True&loc=Local", dbString, dbName))
+	if err != nil {
+		fmt.Printf("%s", err)
 		panic(err)
 	}
 	db = d
